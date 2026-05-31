a89771c07cac7d25a2f33297d499a79fdee3a561
braided <leybavlad@gmail.com>
2026-05-31 21:15:11 +0200

    Refactor code style: unify quotation marks and remove unused CSS rules; update HTML files for consistency

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
-const catalog = document.querySelector("#categories");
-const item = document.querySelectorAll(".item>ul");
-const itemH2 = document.querySelectorAll(".item>h2");
+const catalog = document.querySelector('#categories');
+const item = document.querySelectorAll('.item>ul');
+const itemH2 = document.querySelectorAll('.item>h2');
 console.log(`Number of categories: ${catalog.children.length}`);
 for (let i = 0; i < catalog.children.length; i++) {
   console.log(`Category: ${itemH2[i].textContent}`);
--- js/task-4.js
+++ js/task-4.js
@@ -1,13 +1,34 @@
-const loginForm = document.querySelector(".login-form");
 
-loginForm.addEventListener("submit", (event) => {
+// v1
+
+// const emailUser = document.querySelector('input[type=email]');
+// const passwordUser = document.querySelector('input[type=password]');
+// const loginForm = document.querySelector('.login-form');
+
+// loginForm.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const email = emailUser.value.trim();
+//   const password = passwordUser.value.trim();
+//   if (email === '' || password === '') {
+//     alert('All form fields must be filled in');
+//   }else{ console.log({ email, password });
+
+//   loginForm.reset();
+// }
+//
+// });
+
+// v2
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', event => {
   event.preventDefault();
   const formData = {
     email: loginForm.elements.email.value.trim(),
     password: loginForm.elements.password.value.trim(),
   };
-  if (formData.email === "" || formData.password === "") {
-    alert("All form fields must be filled in");
+  if (formData.email === '' || formData.password === '') {
+    alert('All form fields must be filled in');
   } else {
     console.log(formData);
     loginForm.reset();
